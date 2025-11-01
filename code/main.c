4e14b4cd148e40f48424355ee8452708a354cb42
Jonas Welle <jonashwelle@proton.me>
2025-11-01 10:18:38 +0100

    Created render pass.

--- code/main.c
+++ code/main.c
@@ -281,7 +281,7 @@ typedef struct Context {
 	VkInstance vk_instance;
 
 	VkPhysicalDevice vk_physical_device;
-	VkPhysicalDeviceProperties vk_physical_device_properties;
+	VkPhysicalDeviceProperties2 vk_physical_device_properties;
 	VkPhysicalDeviceMemoryProperties vk_physical_device_memory_properties;
 
 	VkSurfaceKHR vk_surface;
@@ -305,6 +305,7 @@ typedef struct Context {
 
 	VkDescriptorSetLayout vk_descriptor_set_layout;
 	VkPipelineLayout vk_pipeline_layout;
+	VkRenderPass vk_render_pass;
 } Context;
 
 #include "util.c"
@@ -1296,7 +1297,7 @@ int32_t main(int32_t argc, char* argv[]) {
 		VK_CHECK(vkEnumeratePhysicalDevices(ctx->vk_instance, &physical_device_count, physical_devices));
 		ctx->vk_physical_device = physical_devices[1];
 
-		vkGetPhysicalDeviceProperties(ctx->vk_physical_device, &ctx->vk_physical_device_properties);
+		vkGetPhysicalDeviceProperties2(ctx->vk_physical_device, &ctx->vk_physical_device_properties);
 		vkGetPhysicalDeviceMemoryProperties(ctx->vk_physical_device, &ctx->vk_physical_device_memory_properties);
 	}
 
@@ -1450,7 +1451,70 @@ int32_t main(int32_t argc, char* argv[]) {
 		VK_CHECK(vkCreatePipelineLayout(ctx->vk_device, &pipeline_layout_info, NULL, &ctx->vk_pipeline_layout));
 	}
 
+	// create_render_pass
+	{
+		VkAttachmentDescription color_attachment = {
+			.format = ctx->vk_swapchain_info.imageFormat,
+			.samples = VK_SAMPLE_COUNT_1_BIT,
+			.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+			.storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+			.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+			.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+			.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR,
+		};
 
+		VkAttachmentReference color_attachment_ref = {
+			.attachment = 0, // index in attachments array
+			.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL,
+		};
+
+		VkAttachmentDescription depth_attachment = {
+			.format = VK_FORMAT_D32_SFLOAT_S8_UINT, // TODO
+			.samples = VK_SAMPLE_COUNT_1_BIT,
+			.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+			.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+			.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+			.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+			.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+		};
+
+		VkAttachmentReference depth_attachment_ref = {
+			.attachment = 1,
+			.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+		};
+
+		VkSubpassDescription subpass = {
+			.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
+			.colorAttachmentCount = 1,
+			.pColorAttachments = &color_attachment_ref,
+			.pDepthStencilAttachment = &depth_attachment_ref,
+		};
+
+		VkSubpassDependency subpass_dependency = {
+			.srcSubpass = VK_SUBPASS_EXTERNAL, // implicit subpass before and after
+			.dstSubpass = 0, // subpass index
+			.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
+			.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
+			.srcAccessMask = 0,
+			.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
+		};
+
+		VkAttachmentDescription attachments[] = { color_attachment, depth_attachment };
+
+		VkRenderPassCreateInfo info = { 
+			.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+			.attachmentCount = SDL_arraysize(attachments),
+			.pAttachments = attachments,
+			.subpassCount = 1,
+			.pSubpasses = &subpass,
+			.dependencyCount = 1,
+			.pDependencies = &subpass_dependency,
+		};
+
+		VK_CHECK(vkCreateRenderPass(ctx->vk_device, &info, NULL, &ctx->vk_render_pass));
+	}
 	
 	// LoadSprites
 	{
