d644e6f65f93b6df3aeadf7902977e1f388d3fdb
Jonas Welle <jonashwelle@proton.me>
2025-12-04 12:24:36 +0100

    Fixed accidentally setting srcAlphaBlendFactor twice.
    In this case, it doesn't actually matter because VK_BLEND_FACTOR_ZERO is 0, but who knows? Maybe I'll want to change the value of these later.

--- code/main.c
+++ code/main.c
@@ -1924,7 +1924,7 @@ int32_t main(int32_t argc, char* argv[]) {
 			.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
 			.colorBlendOp = VK_BLEND_OP_ADD,
 			.srcAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
-			.srcAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
+			.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
 			.alphaBlendOp = VK_BLEND_OP_ADD,
 			.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT,
 		};
