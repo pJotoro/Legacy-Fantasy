1e85e9cd85ac5706bbcc3adc81274094fa7de862
Jonas Welle <jonashwelle@proton.me>
2026-08-21 12:16:02 +0200

    Cleaned up the CMake build system.

--- code/main.c
+++ code/main.c
@@ -5,16 +5,12 @@ It's clean up time! Here are some things I want to do with this codebase before 
 - Preprocessor macros
 - Naming conventions
 - Vulkan initialization (things like disabling features I know I won't use)
-- CMake build system.
+# CMake build system.
 
 2. Clean up the damn code! I mean seriously, this code is very messy.
 - Keep everything inside of one file (except aseprite declarations). Normally splitting things up into multiple files is good, but for a codebase this small, I would say it makes it a lot harder to follow what the heck is going on.
 - Strenously domument everything.
 - Eliminate commented out code.
-
-3. Eliminate anything that isn't strictly necessary.
-- Replace cglm with custom vector types.
-- Replace SDL with the Windows API.
 */
 
 #define TOGGLE_PROFILING 0
